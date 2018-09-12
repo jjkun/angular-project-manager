4b57b482210e40bf8916f71f46f89b92703dbea1
jperea <jperea@mailprocp.com>
2018-09-11 19:12:05 -0500

    Header, Modelo y Serivicios

---
+++ src/app/auth/project-list/project-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ProjectListService } from './services/project-list.service';
+import { Project } from './models/project.model';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.css']
+})
+export class ProjectListComponent implements OnInit {
+
+  isLoading = true;
+  projects: Array<Project>
+
+  constructor(private _projectListService: ProjectListService) { }
+
+  ngOnInit() {
+    this._projectListService.getAll().subscribe(
+      (data: Project[]) =>{
+        this.projects = data;
+        this.isLoading = false;
+      },
+      err => {
+        console.error(err);
+      }, 
+      () => {
+        console.log("Finished");
+      }
+    );
+  }
+
+}
---
+++ src/app/auth/project-list/services/project-list.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project.model';
+
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class ProjectListService {
+    projects: Array<Project> = [];
+    constructor(private _http: Http){
+    }
+
+    getAll() : Observable<Array<Project>>{
+        const url = 'http://172.104.91.187/projects';
+        const headers = new Headers({'Content-Type':'application/json'});
+        const options = new RequestOptions({headers:headers});
+        return this._http.get(url,options).pipe(
+            map((response) => {
+                return  response.json()
+            }
+        ));
+        //next()
+        //error()
+        //onFinished
+        //Http Request Open connection ---> Server
+        //UI <--- Response
+    }
+}
